63ad447e10a249b4d87d39d15ca158d14b5c345b
-hasty- <tikhomirovtimofei@gmail.com>
2022-01-05 00:14:35 +0300

    Add time and battery blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,14 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0}, */
-    {"", "/home/hasty/.local/scripts/statusbar/time",					1,		1},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    /* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
+       1, 0}, */
+    {"", "/home/hasty/.local/scripts/statusbar/time", 1, 1},
+    {"", "/home/hasty/.local/scripts/statusbar/battery", 5, 2},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 2;
