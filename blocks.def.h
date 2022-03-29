9a653e6e59177aec28baa972b5f9e11ca05d07ba
-hasty- <tikhomirovtimofei@gmail.com>
2022-03-30 00:40:03 +0300

    Use tilde expansion in home path

--- blocks.def.h
+++ blocks.def.h
@@ -4,8 +4,8 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     /* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
        1, 0}, */
-    {"", "/home/hasty/.local/scripts/statusbar/time", 1, 1},
-    {"", "/home/hasty/.local/scripts/statusbar/battery", 5, 2},
+    {"", "~/.local/scripts/statusbar/time", 1, 1},
+    {"", "~/.local/scripts/statusbar/battery", 5, 2},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
